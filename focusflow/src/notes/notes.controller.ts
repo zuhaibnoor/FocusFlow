f0f7a889591e06ec91f46a49f092190a9a3527df
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-12-30 19:44:07 +0500

    Added notes controller

--- focusflow/src/notes/notes.controller.ts
+++ focusflow/src/notes/notes.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { NotesService } from './notes.service';
 
 @Controller('notes')
-export class NotesController {}
+export class NotesController {
+    constructor(private readonly notesService: NotesService){}
+
+    @Get()
+    findAll(){
+        return this.notesService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return this.notesService.findOne(Number(id));
+    }
+    
+    @Delete(':id')
+    delete(@Param('id') id:string){
+        return this.notesService.delete(Number(id));
+    }
+    
+    @Post()
+    create(@Body() body : {title: string, content: string}){
+        return this.notesService.create(body.title, body.content);
+    }
+
+    @Put(':id')
+    update(@Param('id') id:string, @Body() body: {title: string, content: string}){
+        return this.notesService.update(Number(id), body.title, body.content);
+    }
+}
--- focusflow/src/notes/notes.service.ts
+++ focusflow/src/notes/notes.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
 
+
+
 @Injectable()
-export class NotesService {}
+export class NotesService {
+    private notes: any[] = []
+
+    private idCounter = 1
+
+    create(title: string, content: string){
+        const note = {
+            id: this.idCounter++,
+            title,
+            content,
+            isCompleted: false,
+        };
+
+        this.notes.push(note)
+        return this.notes;
+    }
+
+    findAll(){
+        return this.notes;
+    }
+
+    findOne(id: number){
+        return this.notes.find(note => note.id === id);
+    }
+
+    update(id:number, title: string, content:string){
+        const note = this.findOne(id);
+        if(note){
+            note.title = title;
+            note.content = content;
+            return note;
+        }
+    }
+
+    delete(id:number){
+        this.notes = this.notes.filter(note => note.id !== id);
+        return this.notes;
+    }
+
+    
+}
